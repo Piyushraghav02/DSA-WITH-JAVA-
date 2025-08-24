d8085aa64e8d86945df9c9a7b5e64b88d07571b0
Piyush raghav <piyushraghav02112002@gmail.com>
2025-08-24 18:22:27 +0530

    Sum finded

--- Recursion/arrrecur.java
+++ Recursion/arrrecur.java
@@ -25,13 +25,25 @@ class arrrecur {
 
     }
 
+    static int calsum(int arr[], int ind) {
+
+        if (ind == arr.length - 1) {
+            return arr[ind];
+        }
+
+        int max = calsum(arr, ind + 1);
+
+        return max + arr[ind];
+
+    }
+
     public static void main(String[] args) {
 
         int arr[] = { 4, 5, 10, 7, 8 };
 
-        System.out.println(calmax(arr, 0));
-
+        // System.out.println(calmax(arr, 0));
         // printarr(arr, 0);
+        System.out.println(calsum(arr, 0));
 
     }
 }
