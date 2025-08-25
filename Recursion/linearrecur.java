995a3680c3b85ac4a472086973e26f862e94ed2a
Piyush raghav <piyushraghav02112002@gmail.com>
2025-08-25 13:53:21 +0530

    Linear search index done

--- Recursion/linearrecur.java
+++ Recursion/linearrecur.java
@@ -10,14 +10,28 @@ public class linearrecur {
 
     }
 
+    static int linearecurIN(int nums[], int ind, int tar) {
+
+        if (ind == nums.length)
+            return -1;
+
+        if (nums[ind] == tar)
+            return ind;
+
+        return linearecurIN(nums, ind + 1, tar);
+
+    }
+
     public static void main(String[] args) {
 
         int nums[] = { 2, 5, 6, 8, 3, 4 };
 
-        if (linearecur(nums, 0, 8))
-            System.out.println("yes its found");
+        // if (linearecur(nums, 0, 8))
+        // System.out.println("yes its found");
 
-        else
-            System.out.println("its not found");
+        // else
+        // System.out.println("its not found");
+
+        System.out.println(linearecurIN(nums, 0, 8));
     }
 }
