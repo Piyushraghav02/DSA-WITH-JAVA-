4256fc4c53f6d1cd28cb42f19d3de1881774aded
Piyush raghav <piyushraghav02112002@gmail.com>
2025-09-04 20:20:02 +0530

    bubble rev done

--- Sorting/revbubble.java
+++ Sorting/revbubble.java
@@ -23,6 +23,24 @@ class revbubble {
         printarr(arr);
     }
 
+    static void OptBubblesor(int arr[]) {
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean flag = false;
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            if (flag == false)
+                return;
+        }
+
+        printarr(arr);
+    }
+
     public static void main(String[] args) {
 
         int nums[] = { 4, 5, 6, 7, 1, 2, 5 };
