6af3e7d094e00a0ff926e8d239af71bf006cf6b5
Piyush raghav <piyushraghav02112002@gmail.com>
2025-09-09 23:41:26 +0530

    count sort done

--- Sorting/count.java
+++ Sorting/count.java
@@ -20,30 +20,30 @@ public class count {
     static void Countsort(int arr[]) {
         int max = findmax(arr);
 
-        int[] carr = new int[max];
+        int[] freqarr = new int[max + 1];
         int k = 0;
 
-        for (int i = 0; i < arr.length - 1; i++) {
-            int count = 0;
-            k = i;
-            for (int j = i; j < arr.length - 1; j++) {
+        for (int i = 0; i < freqarr.length - 1; i++) {
 
-                if (arr[j] == i) {
-                    count++;
-                }
-
-            }
-            if (count > 0)
-                carr[k++] = count;
-            else
-                 carr[i] = 0;
+            freqarr[arr[i]]++;
         }
 
-        printarr(carr);
+        for (int i = 0; i < freqarr.length - 1; i++) {
+            for (int j = 0; j < freqarr[i]; j++) {
+                arr[k++] = i;
+            }
+        }
+
+        printarr(freqarr);
     }
 
     public static void main(String[] args) {
-        int arr2[] = { 4, 1, 6, 2, 7, 9, 3, 4, 6, 8, 3, 1, };
-        Countsort(arr2);
+        int arr[] = { 4, 1, 6, 2, 7, 9, 3, 4, 6, 8, 3, 1, };
+
+        System.out.print("This is original array :- ");
+        printarr(arr);
+        System.out.println();
+        System.out.print("This is count array :- ");
+        Countsort(arr);
     }
 }
