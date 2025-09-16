b94beca84e3b8b5ed342b4383f656d829ff6426f
Piyush raghav <piyushraghav02112002@gmail.com>
2025-09-16 23:49:27 +0530

    Linkedlist started

---
+++ Linkedlist/Linkedlist.java
@@ -0,0 +1,63 @@
+
+public class Linkedlist {
+
+    public class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node Start, End;// initalize the Pointer which is type of Node (hold the add of nodes)
+
+    public void Addfirst(int data) {
+        Node head = new Node(data); // This is ADD First Method
+
+        if (head == null) {
+            Start = End = head;
+            return;
+        } else {
+            head.next = Start;
+            Start = head;
+            System.out.println("Node Inserted succesfully");
+        }
+    }
+
+    public void Addlast(int data) {
+        Node head = new Node(data); // This is Insert LAST Method
+
+        if (head == null) {
+            Start = End = head;
+            return;
+        } else {
+            End.next = head;
+            End = head;
+        }
+    }
+
+    public void Display(Node Start) {
+        if (Start == null) {
+            System.out.println("Linked list is Empty");
+        } else {
+            Node temp = Start;
+            while (temp != null) {
+                System.out.print(temp.data + " -> ");
+                temp = temp.next;
+            }
+
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        Linkedlist LL = new Linkedlist();
+
+        LL.Addfirst(10);
+        LL.Addfirst(5);
+LL.Addlast(20);
+        LL.Display(Start);
+    }
+}
