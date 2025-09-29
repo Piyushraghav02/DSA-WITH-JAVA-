3fd158dfc4ec4b051ce2c97c1d05a1cd50d9ee66
Piyush raghav <piyushraghav02112002@gmail.com>
2025-09-29 23:59:11 +0530

    not done something today

--- Stack/basics.java
+++ Stack/basics.java
@@ -10,11 +10,21 @@ public class basics {
         st.push(4);
         st.push(5);
         System.out.println(st);
-        
+
+        int ind = 2;
+        int val = 6;
+
         Stack<Integer> st2 = new Stack<>();
-        while (st.size() > 0) {
+        
+        while (st.size() >ind) {
             st2.push(st.pop());
         }
-        System.out.println(st2);
+        st.push(val);
+
+        while (st2.size() > 0) {
+            st.push(st2.pop());
+        }
+        System.out.println(st);
+        
     }
 }
