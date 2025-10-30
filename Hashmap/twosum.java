1323cec9bceb686f588a74b64b5a7035407af449
Piyush raghav <piyushraghav02112002@gmail.com>
2025-10-30 22:28:06 +0530

    occurance and max on table done

--- Hashmap/twosum.java
+++ Hashmap/twosum.java
@@ -3,32 +3,37 @@ import java.util.HashMap;
 
 public class twosum {
     
-
-    public static void main(String[] args) {
-        int arr[] = { 1, 5, 7, -1 };
-
-        int ind=-1,ind2=-1;
-
-        int tar = 6;
+    public static int[] Findtwosum(int arr[],int tar) {
+    
+        int arr2[] = { -1 };
 
         HashMap<Integer,Integer> h1=new HashMap<>();
 
         for (int i = 0; i < arr.length; i++) {
-            
-                int findtar=tar-arr[i];
 
-                if (h1.containsKey(findtar)) {
-                    ind = i;
-                    ind2 = h1.get(findtar);
-                }
-            else
-                h1.put(arr[i], i);
-        
+            int findtar = tar - arr[i];
+
+            if (h1.containsKey(findtar)) {
+                arr2 = new int[] { i, h1.get(findtar) };
+                return arr2;
+            }
+            h1.put(arr[i], i);
+
         }
+        return arr2;
+
+}
+
+    public static void main(String[] args) {
         
-`       System.out.println("These are the index : -" +);
+        int arr[] = { 1, 5, 7, -1 };
+    
+        int arr2[] = Findtwosum(arr, 6);
 
-
+        for (Integer i : arr2) {
+            System.out.println(i);
+        }
+    
 
     }
 }
