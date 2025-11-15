1ca0feda6b72760bfe263dcc01d4b0f372a46906
Piyush raghav <piyushraghav02112002@gmail.com>
2025-11-15 23:56:50 +0530

    lc 53 max sum subarr done

--- leet code/LC_724_findpivot.java
+++ leet code/LC_724_findpivot.java
@@ -22,9 +22,8 @@ public class LC_724_findpivot {
     public static void main(String[] args) {
         int nums[] = { 1, 7, 3, 6, 5, 6 };
 
-        System.out.println("This is the pivot :- "+pivotIndex(nums));
-
-
-
+        int max = 
+        System.out.println("This is the pivot :- " + pivotIndex(nums));
+        
     }
 }
