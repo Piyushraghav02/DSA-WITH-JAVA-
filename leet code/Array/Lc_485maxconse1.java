c83262322505a46c4e7013d6f12941a5b009aea5
Piyush raghav <piyushraghav02112002@gmail.com>
2026-02-04 23:52:11 +0530

    revise one question

--- leet code/Array/Lc_485maxconse1.java
+++ leet code/Array/Lc_485maxconse1.java
@@ -4,28 +4,41 @@ public class Lc_485maxconse1 {
     public static int findMaxConsecutiveOnes(int[] nums) {
         int n = nums.length;
         
-        if (n == 0 && nums[0] == 0)
-            return 0;
+        // if (n == 0 && nums[0] == 0)
+        //     return 0;
         
-        int max = 1, count = 1;
+        // int max = 1, count = 1;
 
-        for (int i = 1; i < n; i++) {
+        // for (int i = 1; i < n; i++) {
 
-            if(nums[0]==0)
-                return 0;
-            else if (nums[i] == 1) {
-                count++;
-                max = Math.max(count, max);
-            } else {
-                count = 1;
+        //     if(nums[0]==0)
+        //         return 0;
+        //     else if (nums[i] == 1) {
+        //         count++;
+        //         max = Math.max(count, max);
+        //     } else {
+        //         count = 1;
+        //     }
+        // }
+        // return max;
+
+        int low = 0, maxlen = Integer.MIN_VALUE;
+        for (int high = 0; high < n; high++) {
+            if (nums[high] == 1) {
+                low++;
             }
+            else {
+                low = 0;
+            }
+            maxlen = Math.max(maxlen, low);
         }
-        return max;
-    }
+        return maxlen;
+        
+     }
     
     public static void main(String[] args) {
         
-        int arr[] = { 0};
+        int arr[] = { 1,1,0,1,1,1,1};
         System.out.println("Max no of consecutive one :- "+findMaxConsecutiveOnes(arr));
     }
 }
