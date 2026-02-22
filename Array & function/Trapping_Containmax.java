a4a1e87cdfb56fc8945302e51582f18a6131b28e
Piyush raghav <piyushraghav02112002@gmail.com>
2026-02-22 15:44:35 +0530

    one done

--- Array & function/Trapping_Containmax.java
+++ Array & function/Trapping_Containmax.java
@@ -29,51 +29,74 @@ public class Trapping_Containmax{
         return trapped;
     }
 
+    public static int trap(int[] height) {
+        int n = height.length;
+        int left[] = new int[n];
+        int right[] = new int[n];
+
+        left[0] = height[0];
+        right[n - 1] = height[n - 1];
+
+        int trappedwater = 0;
+
+        for (int i = 1; i < n; i++) {
+            left[i] = Math.max(left[i - 1], height[i]);
+        }
+
+        for (int j = n - 2; j >= 0; j--) {
+            right[j] = Math.max(right[j+1], height[j ]);
+        }
+
+        for (int i = 0; i < n; i++) {
+            trappedwater += (Math.min(right[i], left[i]) - height[i]);
+        }
+        return trappedwater;
+    }
+    
     public static int Containmax(int arr[]) {
 
         int n = arr.length - 1;
 
-        int low = 0, high = n;
+        int left = 0, right = n;
 
         int maxwater = Integer.MIN_VALUE;
 
-        while (low <= high) {
+        while (left <= right) {
 
-            int minheight = Math.min(arr[low], arr[high]);
+            int minheight = Math.min(arr[left], arr[right]);
 
-            int currwater = (high - low) * minheight;
+            int currwater = (right - left) * minheight;
 
             maxwater = Math.max(currwater, maxwater);
 
-            if (arr[low] < arr[high]) {
-                low++;
+            if (arr[left] < arr[right]) {
+                left++;
             } else {
-                high--;
+                right--;
             }
         }
 
         return maxwater;
     }
 
-
     public static int maxArea(int[] height) {
         int n = height.length;
 
-        int low = 0, high = n - 1;
+        int left = 0, right = n - 1;
 
         int maxwater=Integer.MIN_VALUE;
-        while (low <=high)
+        while (left <=right)
         {
-            int minheight = Math.min(height[low], height[high]);
+            int minheight = Math.min(height[left], height[right]);
 
-            int currwater = (high-low) * minheight;
+            int currwater = (right-left) * minheight;
 
             maxwater = Math.max(currwater, maxwater);
 
-            if (height[low] < height[high]) {
-                low++;
+            if (height[left] < height[right]) {
+                left++;
             } else {
-                high--;
+                right--;
             }
         }
         return maxwater;
@@ -84,8 +107,10 @@ public class Trapping_Containmax{
 
         //int height[] = { 2, 3, 4, 6, 2, 5, 18 };
 
-        int height[] = { 1, 8, 6, 2, 5, 4, 8, 3, 7 };
+        int height[] = { 0,1,0,2,1,0,1,3,2,1,2,1 };
 
-        System.out.println("Maximum water is :- "+maxArea(height));
+        System.out.println("Maximum water is :- " + trap(height));
+        
+        
     }
 }
