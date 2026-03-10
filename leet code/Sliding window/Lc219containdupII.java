f4e3d7a8c122f38e9b09ab72652fdc274ac583d6
Piyush raghav <piyushraghav02112002@gmail.com>
2026-03-10 21:34:19 +0530

    not done yet

---
+++ leet code/Sliding window/Lc219containdupII.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Lc219containdupII {
+    
+    public static boolean containsNearbyDuplicate(int[] nums, int k) {
+        int n = nums.length;
+
+        Set<Integer> hs = new HashSet<>();
+        
+        int low = 0;
+        boolean flag = false;
+
+        for(int high = 0; high < n; high++){
+
+            // check duplicate first
+            if(hs.contains(nums[high])){
+                return true;
+            }
+
+            hs.add(nums[high]);
+
+            // shrink window if size > k
+            while(high - low >= k){
+                hs.remove(nums[low]);
+                low++;    
+            }
+        }
+
+        return flag;
+    }
+
+    public static void main(String[] args) {
+        int nums[] = {1,2,3,1,2,3};
+        System.out.println(containsNearbyDuplicate(nums, 2));
+    }
+}
