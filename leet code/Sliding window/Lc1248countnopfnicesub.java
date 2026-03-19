3f5aee9ad3d82d845c2881825dbba83bc6097b6a
Piyush raghav <piyushraghav02112002@gmail.com>
2026-03-19 20:50:40 +0530

    done something

--- leet code/Sliding window/Lc1248countnopfnicesub.java
+++ leet code/Sliding window/Lc1248countnopfnicesub.java
@@ -3,37 +3,37 @@ import java.util.Map;
 
 public class Lc1248countnopfnicesub {
     
-    
-    public static int numberOfSubarrays(int[] nums, int k) {
+    public static int calnumberOfSubarrays(int[] nums, int k) {
         int n = nums.length;
+        
+        int low = 0, count = 0, sum = 0;
 
-        int low = 0, count = 0, totlsub = 0;
+        if (k < 0)
+            return 0;
 
-        Map<Integer, Integer> hm = new HashMap<>();
-
-        for (int high = 0; high < n; high++) {
-
-            hm.put(nums[high] % 2, hm.getOrDefault(nums[high] % 2, 0) + 1);
-
-            if (hm.containsKey(1) && hm.get(1) <= k) {
-                count++;
-            } else {
-                if(count==k)
-                    totlsub++;
-            }
-
-            while (count == k && low <= high) {
-                hm.put(nums[high] % 2, hm.getOrDefault(nums[high] % 2, 0) - 1);
+        for(int high=0;high<n;high++) {
+            sum += nums[high] % 2;
+            
+            while (sum >k) {
+                sum -= nums[low] % 2;
+                
                 low++;
             }
+            count += high - low + 1;
+            
         }
-        return totlsub;
+        return count;
     }
     
+    public static int numberOfSubarrays(int[] nums, int k) {
+        return calnumberOfSubarrays(nums, k) - calnumberOfSubarrays(nums, k - 1);
+    }
     
     public static void main(String[] args) {
     
+        int nums[] = { 1, 1, 2, 1, 1 };
         
+        System.out.println(numberOfSubarrays(nums, 3));    
 
 
     }
