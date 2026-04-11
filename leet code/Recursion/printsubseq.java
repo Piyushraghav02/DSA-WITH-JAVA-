3fbe29a396a695ac2e473ce94d704c6892bde0b6
Piyushraghav02 <Piyushraghav02112002@gmail.com>
2026-04-11 23:57:13 +0530

    going again forward to recursion

--- leet code/Recursion/printsubseq.java
+++ leet code/Recursion/printsubseq.java
@@ -3,7 +3,7 @@ import java.util.List;
 
 public class printsubseq {
 
-    public static void printingsubseq(int arr[],int i,List<Integer> l,int sum,int k) {
+    public static void printingsubseq(int arr[], int i, List<Integer> l, int sum, int k) {
         if (i == arr.length) {
             if (sum == k) {
                 System.out.println(l);
@@ -14,20 +14,66 @@ public class printsubseq {
         l.add(arr[i]);
         sum += arr[i];
         printingsubseq(arr, i + 1, l, sum, k);
+
+        sum -= arr[i];
+        l.remove(l.size() - 1);
+
+        //not take
+        printingsubseq(arr, i + 1, l, sum, k);
+    }
+    
+    public static int countprintingsubseq(int arr[], int i, int sum, int k) {
+        if (i == arr.length) {
+            if (sum == k) {
+                return 1;
+            }
+            return 0;
+        }
+        //take
+      
+        sum += arr[i];
+        int left=countprintingsubseq(arr, i + 1, sum, k);
+
+        sum -= arr[i];
+    
+        //not take
+        int right = countprintingsubseq(arr, i + 1, sum, k);
+        
+        return left + right;
+    }
+
+    public static boolean printingsubseqonlyone(int arr[],int i,List<Integer> l,int sum,int k) {
+        if (i == arr.length) {
+            if (sum == k) {
+                System.out.println(l);
+                return true;
+            } 
+            else
+            return false;
+        }
+        //take
+        l.add(arr[i]);
+        sum += arr[i];
+        
+        if (printingsubseqonlyone(arr, i + 1, l, sum, k) == true) {
+            return true;
+        }
        
         sum -= arr[i];
         l.remove(l.size() - 1);
     
         //not take
-        printingsubseq(arr, i + 1, l,sum,k);
+        if (printingsubseqonlyone(arr, i + 1, l, sum, k) == true) {
+            return true;
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        List<Integer> l = new ArrayList<>();
+      
+        int arr[] = { 1, 2, 1 };
 
-        int arr[] = { 3, 1, 2 };
-
-        printingsubseq(arr,0,l,0,4);
+      System.out.print(countprintingsubseq(arr,0, 0,2));
 
 
         
