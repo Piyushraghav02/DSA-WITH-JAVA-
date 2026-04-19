fff948be2e6faccf5629f5487a697eb2bc035b89
Piyushraghav02 <Piyushraghav02112002@gmail.com>
2026-04-19 16:13:04 +0530

    good

--- Leetcode/Recursion/Lc39.java
+++ Leetcode/Recursion/Lc39.java
@@ -5,37 +5,33 @@ import java.util.Set;
 
 public class Lc39 {
     
-    public static HashSet<List<Integer>> hs = new HashSet<>();
+  
 
     public static void calcombinationsum(int arr[],int i,List<Integer> l,List<List<Integer>> ans,int tar){
 
-        if(i==arr.length || tar<0)return;
         if (tar == 0) {
-            if (!hs.contains(l)) {
-                ans.add(new ArrayList<>(l));
-                hs.add(l);
-           }
+            ans.add(new ArrayList<>(l));
             return;
-       }
-        //for the single inclusion 
+        }
+        if (i == arr.length || tar < 0)
+            return;
+
+        //pick 
         l.add(arr[i]);
-        calcombinationsum(arr,i+1,l,ans,tar-arr[i]);
-    
-        //for multiple execlution
-         calcombinationsum(arr,i,l,ans,tar-arr[i]);
-    
-        //for the exclustion
-        l.remove(l.size()-1);
-        calcombinationsum(arr,i+1,l,ans,tar);
-    
+        calcombinationsum(arr, i, l, ans, tar - arr[i]);
+        
+        //not pick
+        l.remove(l.size() - 1);
+        calcombinationsum(arr, i+1, l, ans, tar);
+       
+        
        }
 
        public static List<List<Integer>> combinationSum(int[] candidates, int target) {
-       
-        List<Integer> curr = new ArrayList<>();   
+    
         List<List<Integer>> ans = new ArrayList<>();
 
-        calcombinationsum(candidates, 0, curr, ans, target);
+        calcombinationsum(candidates, 0, new ArrayList<>(), ans, target);
         return ans;
 
        }
